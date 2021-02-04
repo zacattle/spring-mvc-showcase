b90f749e6fe13a2163628332681cc76121782678
zacattle <yaowangbeyond2010@163.com>
2021-02-04 18:46:17 +0800

    no msg

--- src/main/java/org/springframework/samples/mvc/simple/SimpleController.java
+++ src/main/java/org/springframework/samples/mvc/simple/SimpleController.java
@@ -3,6 +3,10 @@ package org.springframework.samples.mvc.simple;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * 简单controller测试
+ *
+ */
 @RestController
 public class SimpleController {
 
